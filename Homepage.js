764685533392398bd18260ef49ec2dc2047cf985
Pratyush Asthana <66863954+pratyush-surge@users.noreply.github.com>
2021-01-10 22:45:58 +0530

    Add files via upload

---
+++ Homepage.js
@@ -0,0 +1,25 @@
+
+function sh(){
+    $("nav").fadeIn();
+};
+function cloz(){
+    $("nav").fadeOut();
+};
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+    });
+
+    $("#send").on("click", function(){
+        $(this).html("Thanks for your feedback !");
+    });
+});
